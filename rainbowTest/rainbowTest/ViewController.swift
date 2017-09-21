94c711a9b19a8be4fccd09e5f856ab1e014afe54
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-21 13:51:19 -0700

    Add main functionality

---
+++ rainbowTest/rainbowTest/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  rainbowTest
+//
+//  Created by Liseth Cardozo Sejas on 9/21/17.
+//  Copyright © 2017 Liseth Cardozo Sejas. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        let color = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+        cell.backgroundColor =  color[indexPath.row]
+        return cell
+    }
+
+
+}
+
